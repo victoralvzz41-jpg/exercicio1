4c9d5966df750fa79634c75123681879e91c175d
victoralvzz41-jpg <victor.alvzz41@gmail.com>
2025-09-29 18:01:39 -0300

    commit

---
+++ calcvolume.py
@@ -0,0 +1,10 @@
+comprimento = 12  # em cm
+largura = 14      # em cm
+altura = 20       # em cm
+volume = comprimento * largura * altura
+print("--- Dimensões da Caixa ---")
+print(f"Comprimento: {comprimento} cm")
+print(f"Largura:     {largura} cm")
+print(f"Altura:      {altura} cm")
+print("--------------------------")
+print(f"O volume da caixa retangular é de {volume} cm³")
